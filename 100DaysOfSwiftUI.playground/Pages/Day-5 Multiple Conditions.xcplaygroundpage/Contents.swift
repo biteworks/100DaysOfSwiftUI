95ae651e295b92fc3c5239926a9b13f67da448b5
Biteworks <twitter@tobiaswilhelm.info>
2023-01-04 10:25:45 +0100

    Lektion 4-5

---
+++ 100DaysOfSwiftUI.playground/Pages/Day-5 Multiple Conditions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let age = 16
+
+if age >= 18 {
+    print("You can vote in the next election.")
+} else {
+    print("Sorry, you're too young to vote.")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+
+if userAge >= 18 || hasParentalConsent == true {
+    print("You can buy the game")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path…")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
---
+++ 100DaysOfSwiftUI.playground/Pages/Day-5 ternary conditional operator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let names = ["Jayne", "Kaylee", "Mal"]
+let crewCount = names.isEmpty ? "No one" : "\(names.count) people"
+print(crewCount)
+
+enum Theme {
+    case light, dark
+}
+
+let theme = Theme.dark
+
+let background = theme == .dark ? "black" : "white"
+print(background)
+
+let now = Date()
+print("Current date/time: \(now)")
